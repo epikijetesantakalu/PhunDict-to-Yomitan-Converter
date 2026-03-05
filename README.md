a5bff762c1c3382ade7c8ef1a8ff6bc6eb26267b
epikijetesantakalu <lilithaoto@gmail.com>
2026-03-06 00:08:40 +0900

    連濁機能等

--- README.md
+++ README.md
@@ -4,10 +4,9 @@ PhunDict for Yomitanは、[@kaeru2193](https://github.com/kaeru2193)が作成し
 つまりネットで雰語の文章を読むときにポップアップ辞書が使えるよ！
 ## 使い方
 1. ダウンロードしたことがない場合は[ここ](https://yomitan.wiki/)からyomitanをダウンロードする。
-2. [このファイル](https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/PhunDict.zip)をダウンロードする。
-3. yomitanの設定のDictionariesのConfigure installed and enabled dictionaries...からimportを選択し、ダウンロードした辞書ファイルを選択する。
-4. 音声機能を使いたい場合はAudioのConfigure Audio Playback Sources...をクリックし、種類がCustom URLでURLが`https://epiku.net/phun-audios/mp3/{reading}.mp3`の項目を設定する。
-5. デフォルトだとShiftホバーで使えるよ！（スマホ版は多分長押し）
-6. Configure installed and enabled dictionaries...の画面からたまにアップデートがないか確認しよう！
+2. 入れていないならPhunSansとPhunDotを[ダウンロード](https://kaeru2193.net/download/font/)して入れる。
+3. yomitan設定ファイルを[ダウンロード](https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/settings.json)し、Yomitanの設定画面のBackupのImport Settingsからインポートする。
+4. デフォルトだとShiftホバーで使えるよ！（スマホ版は多分長押し）
+5. Configure installed and enabled dictionaries...の画面からたまにアップデートがないか確認しよう！
 ## 一緒に使うことをおすすめする辞書
 - [TuoDict For Yomitan](https://github.com/epikijetesantakalu/TuoDict-for-Yomitan/)
--- main.js
+++ main.js
@@ -13,7 +13,7 @@ async function getDictData(url) {
 
         const result = await response.json();
         return result;
-        
+
     } catch (error) {
         console.error(error.message);
     }
@@ -26,18 +26,34 @@ const dictionary = new Dictionary({
 });
 
 const index = new DictionaryIndex()
-.setTitle("PhunDict")
-.setRevision("1.0.3")
-.setAuthor(dictData.author)
-.setDescription("かえるさんの雰和辞典をyomitan形式に変換したもの")
-.setAttribution("CC BY-NC 4.0")
-.setUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan")
-.setIndexUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/index.json")
-.setDownloadUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/PhunDict.zip")
-.setIsUpdatable(true)
-.build();
+    .setTitle("PhunDict")
+    .setRevision("1.0.3")
+    .setAuthor(dictData.author)
+    .setDescription("かえるさんの雰和辞典をyomitan形式に変換したもの")
+    .setAttribution("CC BY-NC 4.0")
+    .setUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan")
+    .setIndexUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/index.json")
+    .setDownloadUrl("https://github.com/epikijetesantakalu/PhunDict-for-Yomitan/releases/latest/download/PhunDict.zip")
+    .setIsUpdatable(true)
+    .build();
 
 await dictionary.setIndex(index, "./dictionary", "index.json");
+
+dictionary.addTag({
+    name: '漢字転写',
+    category: 'misc',
+    sortingOrder: -5,
+    notes: '漢字転写',
+    popularityScore: 0,
+});
+
+dictionary.addTag({
+    name: '画数',
+    category: 'misc',
+    sortingOrder: -5,
+    notes: '画数',
+    popularityScore: 0,
+});
 
 for (let i = 0; i < dictData.data.length; i++) {
     for (let j = 0; j < dictData.data[i].mean.length; j++) {
@@ -53,11 +69,11 @@ for (let i = 0; i < dictData.data.length; i++) {
                     ]
                 }
             }
-            
+
             const entry = new TermEntry(dictData.data[i].word)
-            .setReading(dictData.data[i].pron.split(" ").join(""))
-            .addDetailedDefinition(detailedDefinition)
-            .build();
+                .setReading(dictData.data[i].pron.split(" ").join(""))
+                .addDetailedDefinition(detailedDefinition)
+                .build();
 
             await dictionary.addTerm(entry);
         }
@@ -65,9 +81,32 @@ for (let i = 0; i < dictData.data.length; i++) {
     generateAudioFile(dictData.data[i].pron.split(" ").join(""));
 }
 
-function generateAudioFile(pronunciation) {
-    execSync(`espeak-ng -vphn ${pronunciation} -w ../phun-audios/wav/${pronunciation}.wav`);
-    execSync(`ffmpeg -i ../phun-audios/wav/${pronunciation}.wav ../phun-audios/mp3/${pronunciation}.mp3 -y`);
+function generateAudioFile(word) {
+    const pronunciation = word
+            .replace(/\d/g, '$& ')
+            .replace(/([snm(ng)])([123])\s(y?[aiueo])/g, (_, p1, p2, p3) => `${p1}${p2} ${p1}${p3}`) //母音の連音
+            .replace(/l([123])\s(y?[aiueo])/g, (_, p1, p2) => `ll${p1} l${p2}`) //dark Lにはならない
+            .replace(/s([123])\s([xqj])/g, (_, p1, p2) => `x${p1} ${p2}`) //x, q, jの連音
+            .replace(/s([123])\sz/g, (_, p1) => `z${p1} z`) //zの連音
+            .split(" ").join("");
+
+    execSync(`espeak-ng -vphn ${pronunciation} -w ../phun-audios/wav/${word}.wav`);
+    execSync(`ffmpeg -i ../phun-audios/wav/${word}.wav ../phun-audios/mp3/${word}.mp3 -y`);
+}
+
+const charaData = await getDictData(charaDictURL);
+
+for (let i = 0; i < charaData.length; i++) {
+    const chara = new KanjiEntry(charaData[i].chara)
+        .setOnyomi(charaData[i].langs.pn.pron)
+        .addMeaning(charaData[i].langs.pn.mean)
+        .setStats({
+            "画数": String(charaData[i].strokes),
+            "漢字転写": charaData[i].chara
+        })
+        .build();
+
+    dictionary.addKanji(chara);
 }
 
 await dictionary.addFile('./styles.css', 'styles.css');
